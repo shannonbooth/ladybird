930a5eaaaee2bf5a2ee0cd61aabde8f0f24445dd
Shannon Booth <shannon@serenityos.org>
2026-04-18 14:16:03 +0200

    LibWeb/Bindings: Factor out function for generating depfiles

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/main.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/main.cpp
@@ -17,6 +17,31 @@
 #include <LibIDL/IDLParser.h>
 #include <LibIDL/Types.h>
 
+static ErrorOr<void> generate_depfile(StringView depfile_path, StringView depfile_prefix, IDL::Parser const& parser, ReadonlySpan<StringView> files)
+{
+    auto depfile = TRY(Core::File::open_file_or_standard_stream(depfile_path, Core::File::OpenMode::Write));
+
+    StringBuilder depfile_builder;
+    for (StringView s : files) {
+        if (s.is_empty())
+            continue;
+
+        if (!depfile_prefix.is_empty())
+            depfile_builder.append(LexicalPath::join(depfile_prefix, s).string());
+        else
+            depfile_builder.append(s);
+
+        break;
+    }
+    depfile_builder.append(':');
+    for (auto const& path : parser.imported_files()) {
+        depfile_builder.append(" \\\n "sv);
+        depfile_builder.append(path);
+    }
+    depfile_builder.append('\n');
+    return depfile->write_until_depleted(depfile_builder.string_view().bytes());
+}
+
 ErrorOr<int> ladybird_main(Main::Arguments arguments)
 {
     Core::ArgsParser args_parser;
@@ -138,28 +163,6 @@ ErrorOr<int> ladybird_main(Main::Arguments arguments)
     TRY(write_if_changed(&IDL::generate_header, header_path));
     TRY(write_if_changed(&IDL::generate_implementation, implementation_path));
 
-    if (!depfile_path.is_empty()) {
-        auto depfile = TRY(Core::File::open_file_or_standard_stream(depfile_path, Core::File::OpenMode::Write));
-
-        StringBuilder depfile_builder;
-        for (StringView s : { header_path, implementation_path }) {
-            if (s.is_empty())
-                continue;
-
-            if (!depfile_prefix.is_empty())
-                depfile_builder.append(LexicalPath::join(depfile_prefix, s).string());
-            else
-                depfile_builder.append(s);
-
-            break;
-        }
-        depfile_builder.append(':');
-        for (auto const& path : parser.imported_files()) {
-            depfile_builder.append(" \\\n "sv);
-            depfile_builder.append(path);
-        }
-        depfile_builder.append('\n');
-        TRY(depfile->write_until_depleted(depfile_builder.string_view().bytes()));
-    }
+    TRY(generate_depfile(depfile_path, depfile_prefix, parser, { { header_path, implementation_path } }));
     return 0;
 }
